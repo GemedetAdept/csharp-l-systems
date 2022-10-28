1db2465f9f049941f46a0439d85ce18ace1c6fe4
Vivian Zitek <vivian.zitek@gmail.com>
2022-10-28 17:51:31 -0600

    Create basic L-system code generator.

--- Program.cs
+++ Program.cs
@@ -9,5 +9,17 @@ string outputFractal = "";
 
 string buildFractal(string axiom, string A, string B) {
 
-	
+	List<string> fractalParts = new List<string>();
+
+	foreach(char i in axiom) {
+
+		if(i == 'A') fractalParts.Add(A);
+		else if(i == 'B') fractalParts.Add(B);		
+	}
+
+	outputFractal = string.Join("", fractalParts.ToArray());
+	return outputFractal;
 }
+
+outputFractal = buildFractal(axiom, ruleA, ruleB);
+Console.WriteLine(outputFractal);
